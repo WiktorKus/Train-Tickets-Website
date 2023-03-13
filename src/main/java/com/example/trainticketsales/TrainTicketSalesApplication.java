c50209c8962d8ae99b25f3df45ea8957f336d667
WiktorKus <wiktorex.kus@gmail.com>
2023-03-13 19:22:44 +0100

    Spring Boot App initialization

---
+++ src/main/java/com/example/trainticketsales/TrainTicketSalesApplication.java
@@ -0,0 +1,13 @@
+package com.example.trainticketsales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainTicketSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainTicketSalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trainticketsales/TrainTicketSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trainticketsales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTicketSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
