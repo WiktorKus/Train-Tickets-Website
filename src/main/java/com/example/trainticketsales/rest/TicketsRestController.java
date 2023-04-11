63b10547d98bbdf9b33f96cfbc8f198efa71213b
WiktorKus <wiktorex.kus@gmail.com>
2023-04-12 00:01:24 +0200

    Endpoints

---
+++ src/main/java/com/example/trainticketsales/rest/TicketsRestController.java
@@ -0,0 +1,49 @@
+package com.example.trainticketsales.rest;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/api")
+public class TicketsRestController {
+
+    @GetMapping("/tickets")
+    public String getTickets() {
+        return "Tickets";
+    }
+
+    @GetMapping("/ticket")
+    public String getTicket() {
+        return "Ticket";
+    }
+
+    @GetMapping("/trains")
+    public String searchTrains(
+            @RequestParam("source") String source,
+            @RequestParam("destination") String destination,
+            @RequestParam("date") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) {
+        return "Search trains";
+    }
+
+    @PostMapping("bookings")
+    public String bookTicket() {
+        return "Bookings";
+    }
+
+    @DeleteMapping("bookings/{bookingId}")
+    public String deleteTicket(@PathVariable String bookingId) {
+        return "Delete";
+    }
+
+    @GetMapping("bookings")
+    public String getBookings() {
+        return "Bookings";
+    }
+
+    @GetMapping("bookings/{bookingId}")
+    public String getBooking(@PathVariable String bookingId) {
+        return "Booking";
+    }
+}
