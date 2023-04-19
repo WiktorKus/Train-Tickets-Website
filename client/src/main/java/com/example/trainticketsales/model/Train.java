0859ca5bc9cb6d98ef4911824d8ba935c3d0bae8
WiktorKus <91941867+WiktorKus@users.noreply.github.com>
2023-04-19 23:07:36 +0200

    Connecting to DB and create tables and related models to the table (#3)

---
+++ client/src/main/java/com/example/trainticketsales/model/Train.java
@@ -0,0 +1,34 @@
+package com.example.trainticketsales.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "train")
+public class Train {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "departure_station", nullable = false)
+    private String departureStation;
+
+    @Column(name = "arrival_station", nullable = false)
+    private String arrivalStation;
+
+    @Column(name = "departure_time", nullable = false)
+    private LocalDateTime departureTime;
+
+    @Column(name = "arrival_time", nullable = false)
+    private LocalDateTime arrivalTime;
+
+}
